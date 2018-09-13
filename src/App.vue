533988eeedda9b8cf79ec435790a95b55f964289
Adrian Gose <adrian@gose.pw>
2018-09-13 01:12:24 -0500

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,67 @@
+<template>
+    <v-app :dark="darktheme">
+
+    <v-toolbar
+      app
+      :clipped-left="clipped"
+    >
+
+      <v-toolbar-title v-text="title"></v-toolbar-title>
+
+    </v-toolbar>
+
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+
+      <v-footer :fixed="fixed" app>
+          &nbsp&nbsp&nbsp
+          <i class="far fa-moon" v-if="!darktheme" @click="darktheme = true"></i>
+          <i class="fas fa-moon" v-else @click="darktheme = false"></i>
+
+          <v-spacer></v-spacer>
+
+          <span>&copy; 2018 Adrian Gose &nbsp&nbsp&nbsp</span>
+      </v-footer>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data () {
+    return {
+        darktheme: false,
+      clipped: false,
+      drawer: true,
+      fixed: false,
+      items: [{
+        icon: 'bubble_chart',
+        title: 'Inspire'
+      }],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Simple Time Clock'
+    }
+  }, beforeMount: function () {
+        if (localStorage.getItem('darktheme') !== null) {
+            var isDark = localStorage.getItem('darktheme');
+
+            this.darktheme = (isDark === 'true');
+        } else {
+            localStorage.setItem('darktheme', this.darktheme);
+        }
+    },
+    watch: {
+        darktheme: function (after, before) {
+            localStorage.setItem('darktheme', after);
+        }
+    }
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,14 @@
+import '@babel/polyfill'
+import Vue from 'vue'
+import './plugins/vuetify'
+import App from './App.vue'
+import store from './store'
+import './registerServiceWorker'
+
+Vue.config.productionTip = false
+Vue.use(require('vue-moment'));
+
+new Vue({
+  store,
+  render: h => h(App)
+}).$mount('#app')
